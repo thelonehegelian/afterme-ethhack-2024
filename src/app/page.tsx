a2dd627b9e8d22b89313db479b3ae9cb03d0e9f5
lonehegelian <thelonehegelian@protonmail.com>
2024-09-02 04:07:32 +0100

    add navber to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <button class="btn btn-primary">Primary</button>
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <button className="btn btn-secondary">Start Now</button>
+        </div>
+      </main>
+    </>
   );
 }
