d7c39b6df564fb43df9bfb84ed37046988c27962
lonehegelian <thelonehegelian@protonmail.com>
2024-09-04 04:08:00 +0100

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,57 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Link from "next/link";
+// import { DM_Serif_Text } from "next/font/google";
+// const dmSerifText = DM_Serif_Text({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <button className="btn btn-secondary">Start Now</button>
+      <main
+        className="flex min-h-screen flex-col items-center justify-between p-24"
+        style={{
+          background: "linear-gradient(360deg, #FFF9D6 0%, #FFFFFF 100%)",
+        }}
+      >
+        <div className="flex items-center justify-center flex-col h-1/2 w-full ">
+          <Image
+            className="mb-10"
+            src="/logo.png"
+            alt="afterMe Logo"
+            width={400}
+            height={400}
+          />
+          <h1
+            className="font-semibold"
+            style={{
+              fontFamily: "DM Serif Text",
+              fontSize: "60px",
+              lineHeight: "72px",
+            }}
+          >
+            <div style={{ color: "#F2CF03", display: "inline" }}>Tomorrow </div>
+            <span style={{ display: "inline" }}>is uncertain, but your</span>
+            <div style={{ color: "#FFA400" }}>
+              Digital legacy{" "}
+              <span style={{ color: "black" }}>does not have to be</span>
+            </div>
+          </h1>
+          <p
+            className="mt-10"
+            style={{
+              fontSize: "1.75rem",
+              fontWeight: 300,
+              lineHeight: "2.5rem",
+            }}
+          >
+            Set up a plan for{" "}
+            <span className="font-semibold italic">after you</span> with{" "}
+            <span className="font-semibold italic">AfterMe</span>
+          </p>
+          <Link href="select-action">
+            <button className="btn btn-secondary mt-10">Start Now</button>
+          </Link>
         </div>
       </main>
     </>
