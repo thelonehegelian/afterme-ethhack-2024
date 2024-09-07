46c9e03d410396b76562fe9660320c3b83085384
annyedge <annyedge@tutamail.com>
2024-09-07 21:36:28 +0200

    REF sign: clean trash

--- afterMeListener.sol
+++ afterMeListener.sol
@@ -1,79 +1,78 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { ISP } from "@ethsign/sign-protocol-evm/src/interfaces/ISP.sol";
-import { Attestation } from "@ethsign/sign-protocol-evm/src/models/Attestation.sol";
-import { DataLocation } from "@ethsign/sign-protocol-evm/src/models/DataLocation.sol";
-
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { ISPHook } from "@ethsign/sign-protocol-evm/src/interfaces/ISPHook.sol";
 
-contract ActuallyMetIRL is Ownable {
+// @dev This contract manages the execution of AfterMe plan. We are separating the AfterMe logic from the hook to make things easier
+// to read.
+contract AfterMeListener is Ownable, ISPHook {
+    // white list is who can be trigger via attestation (eg insurance company)
+    mapping(address attester => bool allowed) public whitelist;
 
-    ISP public spInstance;
+    constructor() Ownable(_msgSender()) { }
 
-    ISPHook public spHookInstance;
-
-    function didReceiveAttestation(address attester, uint64 schemaId, uint64 attestationId, bytes extraData) external payable
-
-    uint64 public schemaId;
-
-    //mapping(address partyA => address partyB) public metIRLMapping;
-
-    //error ConfirmationAddressMismatch();
-
-    //event DidMeetIRL(address partyA, address partyB, uint64 attestationId);
-
-    //constructor() Ownable(_msgSender()) { }
-
-    function setSPInstance(address instance) external onlyOwner {
-        spInstance = ISP(instance);
+    function setWhitelist(address attester, bool allowed) external onlyOwner {
+        whitelist[attester] = allowed;
     }
 
-    function setSchemaID(uint64 schemaId_) external onlyOwner {
-        schemaId = schemaId_;
+    function _checkAttesterAndExecuteAfterMePlan(address attester) internal view {
+        // solhint-disable-next-line custom-errors
+        require(whitelist[attester], 'UnauthorizedAttester');
+
+        // ... start doing after me things
     }
 
-    /*
-    function claimMetIRL(address partyB) external {
-        metIRLMapping[_msgSender()] = partyB;
+    // Sign procotol stuff
+    function didReceiveAttestation(
+        address attester,
+        uint64, // schemaId
+        uint64, // attestationId
+        bytes calldata // extraData
+    )
+        external
+        payable
+    {
+        _checkAttesterAndExecuteAfterMePlan(attester);
     }
-    */
 
+    function didReceiveAttestation(
+        address attester,
+        uint64, // schemaId
+        uint64, // attestationId
+        IERC20, // resolverFeeERC20Token
+        uint256, // resolverFeeERC20Amount
+        bytes calldata // extraData
+    )
+        external
+        view
+    {
+        _checkAttesterAndExecuteAfterMePlan(attester);
+    }
 
-    function confirmMetIRL(address partyA, bytes memory data) external returns (uint64) {
+    function didReceiveRevocation(
+        address attester,
+        uint64, // schemaId
+        uint64, // attestationId
+        bytes calldata // extraData
+    )
+        external
+        payable
+    {
+        _checkAttesterAndExecuteAfterMePlan(attester);
+    }
 
-        // address partyB = _msgSender();
-        if (metIRLMapping[partyA] == partyB) {
-            // B has confirm A's claim of having met them IRL
-            // We now make an attestation of having actually met IRL
-            bytes[] memory recipients = new bytes[](2);
-            recipients[0] = abi.encode(partyA);
-            recipients[1] = abi.encode(partyB);
-
-            Attestation memory a = Attestation({
-                schemaId: schemaId,
-                linkedAttestationId: 0,
-                attestTimestamp: 0,
-                revokeTimestamp: 0,
-                attester: address(this),
-                validUntil: 0,
-                dataLocation: DataLocation.ONCHAIN,
-                revoked: false,
-                recipients: recipients,
-                data: data // SignScan assumes this is from `abi.encode(...)`
-            });
-
-            uint64 attestationId = spInstance.attest(a, "", "", "");
-
-            spInstance.
-
-            emit DidMeetIRL(partyA, partyB, attestationId);
-
-            return attestationId;
-        } else {
-            revert ConfirmationAddressMismatch();
-        }
-
+    function didReceiveRevocation(
+        address attester,
+        uint64, // schemaId
+        uint64, // attestationId
+        IERC20, // resolverFeeERC20Token
+        uint256, // resolverFeeERC20Amount
+        bytes calldata // extraData
+    )
+        external
+        view
+    {
+        _checkAttesterAndExecuteAfterMePlan(attester);
     }
 }
