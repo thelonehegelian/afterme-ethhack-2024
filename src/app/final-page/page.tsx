d1361be6d00feb8095e40e81bcb54092085333fa
lonehegelian <thelonehegelian@protonmail.com>
2024-09-08 14:54:24 +0100

    update styles

--- src/app/final-page/page.tsx
+++ src/app/final-page/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 const FinalPage = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Contract Created</h1>
-      <p className="mb-2">Legacy Locked</p>
-      <p className="mb-2">Your AfterMe Contract and Attestation was created</p>
-      <p className="mb-4">Please go to the Dashboard</p>
-      <a href="/dashboard" className="text-blue-500 underline">
-        Go to Dashboard
-      </a>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center p-4">
+        <h1 className="text-2xl font-bold mb-4">Contract Created</h1>
+        <p className="mb-2">Legacy Locked</p>
+        <p className="mb-2">
+          Your AfterMe Contract and Attestation was created
+        </p>
+        <p className="mb-4">Please go to the Dashboard</p>
+        <Link href="/dashboard">
+          <button className="btn btn-primary">Go to Dashboard</button>
+        </Link>
+      </div>
     </div>
   );
 };
