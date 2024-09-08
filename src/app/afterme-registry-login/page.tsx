19c0bcfeaf184596746208ff1f5aea89fb5fe0ac
lonehegelian <thelonehegelian@protonmail.com>
2024-09-08 16:33:32 +0100

    remove everthing and keep landing page

--- src/app/afterme-registry-login/page.tsx
+++
@@ -1,9 +0,0 @@
-import AfterMeRegistryLanding from "../../components/login-page";
-
-export default function AfterMeRegistryLoginPage() {
-  return (
-    <>
-      <AfterMeRegistryLanding />
-    </>
-  );
-}
--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -1,13 +1,10 @@
 "use client";
 import Image from "next/image";
-import Navbar from "./Navbar";
 import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <>
-      <Navbar />
-
       <main
         className="flex min-h-screen flex-col items-center justify-between p-24"
         style={{
